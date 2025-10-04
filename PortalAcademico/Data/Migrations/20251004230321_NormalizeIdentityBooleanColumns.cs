17b8e2cea8a049ba9f517d90097c652ef6804bc3
Edwin Miguel <miguegara20@gmail.com>
2025-10-04 18:08:19 -0500

    chore: normalize identity boolean columns on postgres

---
+++ PortalAcademico/Data/Migrations/20251004230321_NormalizeIdentityBooleanColumns.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortalAcademico.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NormalizeIdentityBooleanColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder.ActiveProvider == "Npgsql.EntityFrameworkCore.PostgreSQL")
+            {
+                migrationBuilder.Sql("""
+ALTER TABLE "AspNetUsers"
+    ALTER COLUMN "EmailConfirmed" TYPE boolean USING CASE WHEN "EmailConfirmed" IN (1, '1', 'true', 't', 'yes', 'y') THEN true ELSE false END,
+    ALTER COLUMN "PhoneNumberConfirmed" TYPE boolean USING CASE WHEN "PhoneNumberConfirmed" IN (1, '1', 'true', 't', 'yes', 'y') THEN true ELSE false END,
+    ALTER COLUMN "TwoFactorEnabled" TYPE boolean USING CASE WHEN "TwoFactorEnabled" IN (1, '1', 'true', 't', 'yes', 'y') THEN true ELSE false END,
+    ALTER COLUMN "LockoutEnabled" TYPE boolean USING CASE WHEN "LockoutEnabled" IN (1, '1', 'true', 't', 'yes', 'y') THEN true ELSE false END;
+""");
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder.ActiveProvider == "Npgsql.EntityFrameworkCore.PostgreSQL")
+            {
+                migrationBuilder.Sql("""
+ALTER TABLE "AspNetUsers"
+    ALTER COLUMN "EmailConfirmed" TYPE integer USING CASE WHEN "EmailConfirmed" THEN 1 ELSE 0 END,
+    ALTER COLUMN "PhoneNumberConfirmed" TYPE integer USING CASE WHEN "PhoneNumberConfirmed" THEN 1 ELSE 0 END,
+    ALTER COLUMN "TwoFactorEnabled" TYPE integer USING CASE WHEN "TwoFactorEnabled" THEN 1 ELSE 0 END,
+    ALTER COLUMN "LockoutEnabled" TYPE integer USING CASE WHEN "LockoutEnabled" THEN 1 ELSE 0 END;
+""");
+            }
+        }
+    }
+}
